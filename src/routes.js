726c4beb27b89e824c4d9640f1a22e30e21ae21c
Thiago Spindola <thiago_spdvr@hotmail.com>
2023-12-05 18:13:02 -0300

    Initial commit

---
+++ src/routes.js
@@ -0,0 +1,17 @@
+import PaginaPadrao from 'components/PaginaPadrao';
+import Categoria from 'pages/Categoria';
+import Home from 'pages/Home';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+export default function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<PaginaPadrao />}>
+          <Route index element={<Home />} />
+          <Route path='/categoria/:nomeCategoria' element={<Categoria />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
